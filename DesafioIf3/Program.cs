b5898e598db449d3b1eff17cf2f15c0f07a71e70
Shernt07 <set.hernandez@ulv.edu.mx>
2022-10-28 16:32:36 -0500

    bndzgf

--- DesafioIf3/Program.cs
+++ DesafioIf3/Program.cs
@@ -7,38 +7,24 @@ namespace DesafioIf3
         public static void Main(string[] args)
         {
             int numero = 0, Suma = 0, Promedio, Contador =0 ;
+            string desicion = "";
+            //Console.WriteLine("Ingrese los valores");
+            //numero = int.Parse(Console.ReadLine());
 
-            //string Comprobacion = "fin";
+            while (desicion != "fin")
+            {
+                Console.Write("Ingrese los valores: ");
+                numero = int.Parse(Console.ReadLine());
 
-            //while (Comprobacion == "fin")
-            //{
-
-            Console.WriteLine("Ingrese los valores");
-
-            numero = int.Parse(Console.ReadLine());
-
-            while (numero > 0 && numero <= 10)
-                {
-                    Console.WriteLine("Ingrese los valores");
-
-                    numero = int.Parse(Console.ReadLine());
-
-                    Suma += numero;
-                    Contador += 1;
-
-                    //Promedio = Suma / Contador;
-                }
-
+                Suma = Suma + numero;
+                Contador += 1;
                 Promedio = Suma / Contador;
-
-                Console.WriteLine($"El promedio del alumno es de: {Promedio}");
-
-                //Console.Write("Para dejar de meter numeros pon fin, de lo contrario cualquier otra tecla");
-
-                Console.Write("fin = parar or otra cosa para seguir");
-                //Comprobacion = Console.ReadLine();
-
-            //}
+                Console.Write("Para terminar escriba fin, si no cualquier otra tecla\n");
+                desicion = (Console.ReadLine());
+                
+            }
+                Promedio = Suma / Contador;
+            Console.WriteLine($"\nEl promedio del alumno es de: {Promedio}\n");
 
             Console.ReadKey();
                 
--- Notas/Observaciones.cs
+++ Notas/Observaciones.cs
@@ -17,7 +17,26 @@ namespace Notas
 
             //Falta los primeros 3 retos
 
-            
+            string desicion;
+            float Calificacion, Acumulado, Promedio, Materias;
+            desicion = "";
+            Materias = 0;
+            Acumulado = 0;
+            Promedio = 0;
+
+            while (desicion != "fin")
+            {
+                Console.Write("Ingrese una Calificacion: ");
+                Calificacion = int.Parse(Console.ReadLine());
+                Materias++;
+                Acumulado = Acumulado + Calificacion;
+                Promedio = Acumulado / Materias;
+                Console.Write("Desea finalizar [fin] sino Enter: ");
+                desicion = (Console.ReadLine());
+            }
+
+            Console.WriteLine("El promedio de " + Materias + " es: " + Promedio);
+            Console.ReadLine();
         }
     }
 }
