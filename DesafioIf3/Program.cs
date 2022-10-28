768c9dfb72a320c674139ade00ba1acef4e06824
Shernt07 <set.hernandez@ulv.edu.mx>
2022-10-28 16:40:48 -0500

    3

--- DesafioIf3/Program.cs
+++ DesafioIf3/Program.cs
@@ -7,21 +7,26 @@ namespace DesafioIf3
         public static void Main(string[] args)
         {
             int numero = 0, Suma = 0, Promedio, Contador =0 ;
-            string desicion = "";
+            string validacion = "";
             //Console.WriteLine("Ingrese los valores");
             //numero = int.Parse(Console.ReadLine());
 
-            while (desicion != "fin")
+            while (validacion != "fin")
             {
-                Console.Write("Ingrese los valores: ");
+                Console.Write("Escriba una calificacion: ");
                 numero = int.Parse(Console.ReadLine());
-
-                Suma = Suma + numero;
-                Contador += 1;
-                Promedio = Suma / Contador;
-                Console.Write("Para terminar escriba fin, si no cualquier otra tecla\n");
-                desicion = (Console.ReadLine());
-                
+                if (numero>0 && numero<= 10)
+                {
+                    Suma = Suma + numero;
+                    Contador += 1;
+                    Promedio = Suma / Contador;
+                    Console.Write("Para terminar escriba fin, si no cualquier otra tecla\n");
+                    validacion = (Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Calificación no valida, debe ser del 1-10");
+                }
             }
                 Promedio = Suma / Contador;
             Console.WriteLine($"\nEl promedio del alumno es de: {Promedio}\n");
